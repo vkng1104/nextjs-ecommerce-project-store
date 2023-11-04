00a5a4db046a5f2a8f9a258b34fe65198f511b67
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-05 01:17:53 +0700

    feat: create query to filter products using query-string package

--- actions/get-products.tsx
+++ actions/get-products.tsx
@@ -1,9 +1,27 @@
+import qs from "query-string";
+
 import { Product } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
-const getProducts = async (): Promise<Product[]> => {
-  const res = await fetch(URL);
+interface Query {
+  isFeatured?: boolean;
+  categoryId?: string;
+  colorId?: string;
+  sizeId?: string;
+}
+
+const getProducts = async (query: Query): Promise<Product[]> => {
+  const url = qs.stringifyUrl({
+    url: URL,
+    query: {
+      isFeatured: query.isFeatured,
+      categoryId: query.categoryId,
+      sizeId: query.sizeId,
+      colorId: query.colorId,
+    },
+  });
+  const res = await fetch(url);
   return res.json();
 };
 
