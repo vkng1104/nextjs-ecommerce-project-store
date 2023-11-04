f188243fe4eeb5b2eb404a6456a855034291ce0a
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-04 23:56:33 +0700

    feat: create customized button and container

---
+++ components/ui/button.tsx
@@ -0,0 +1,37 @@
+import { forwardRef } from "react";
+
+import { cn } from "@/lib/utils";
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, children, disabled, type = "button", ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(
+          `
+          bg-black 
+          text-white
+          font-semibold
+          w-auto 
+          rounded-full 
+          border-transparent 
+          px-5 py-3 
+          disabled:cursor-not-allowed
+          disabled:opacity-50
+          hover:opacity-75          
+          transition`,
+          className
+        )}
+      >
+        {children}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export default Button;
