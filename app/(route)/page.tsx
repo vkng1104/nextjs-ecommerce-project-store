9db4073f76e129a3ce8781ea8d16558b370cb116
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-05 00:25:08 +0700

    feat: create billboards and update homepage

--- app/(route)/page.tsx
+++ app/(route)/page.tsx
@@ -1,5 +1,18 @@
-const HomePage = () => {
-    return null;
-}
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/ui/billboard";
+
+import Container from "@/components/ui/container";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("e717cb7f-1956-47f8-9499-103573e72b4e");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10"></div>
+      <Billboard data={billboard} />
+    </Container>
+  );
+};
 
 export default HomePage;
