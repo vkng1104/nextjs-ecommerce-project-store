1b11a111e4d8a46355897f9b6c30fde258297e80
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-04 23:55:10 +0700

    feat: create navbar

---
+++ components/navbar/main-nav.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+import { Category } from "@/types";
+
+interface MainNavProps {
+  data: Category[];
+}
+
+const MainNav: React.FC<MainNavProps> = ({ data }) => {
+  const pathname = usePathname();
+
+  const routes = data.map((route) => ({
+    href: `/category/${route.id}`,
+    label: route.name,
+    active: pathname === `/category/${route.id}`,
+  }));
+
+  return (
+    <div className="flex items-center mx-6 space-x-4 lg:space-x-6">
+      {routes.map((route) => (
+        <Link
+          key={route.href}
+          href={route.href}
+          className={cn(
+            "text-sm font-medium transition-colors hover:text-primary",
+            route.active
+              ? "text-black dark:text-white"
+              : "text-muted-foreground"
+          )}
+        >
+          {route.label}
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export default MainNav;
---
+++ components/navbar/navbar.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import Container from "@/components/ui/container";
+import MainNav from "@/components/navbar/main-nav";
+import getCategories from "@/actions/get-categories";
+import NavbarAction from "@/actions/navbar-actions";
+
+export const revalidate = 0;
+
+const Navbar = async () => {
+  const categories = await getCategories();
+  return (
+    <div>
+      <Container>
+        <div className="relative h-16 flex items-center px-4 sm:px-6 lg:px-8">
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+            <p className="font-bold text-xl">STORE</p>
+          </Link>
+          <MainNav data={categories} />
+          <NavbarAction />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+export default Navbar;
