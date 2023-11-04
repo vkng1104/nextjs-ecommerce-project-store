0b1de449464f83d76764aab01eb7e5df2b33ad80
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-05 01:13:38 +0700

    feat: create method to fetch products from admin dashboard

---
+++ actions/get-products.tsx
@@ -0,0 +1,10 @@
+import { Product } from "@/types";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
+const getProducts = async (): Promise<Product[]> => {
+  const res = await fetch(URL);
+  return res.json();
+};
+
+export default getProducts;
--- types.ts
+++ types.ts
@@ -9,3 +9,32 @@ export interface Category {
   name: string;
   billboard: string;
 }
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Product {
+  id: string;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  isArchived: boolean;
+  images: Image[];
+  category: Category;
+  size: Size;
+  color: Color;
+}
