b1eab4428ad972c33521704ef92271fcc5fa5339
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-05 10:54:52 +0700

    refactor: update some components

--- components/ui/no-results.tsx
+++ components/ui/no-results.tsx
@@ -1,5 +1,7 @@
 const NoResults = () => {
-  return <div className="flex items-center"></div>;
+  return (
+    <div className="flex items-center text-gray-500">No results found</div>
+  );
 };
 
 export default NoResults;
