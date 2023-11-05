21df22a7c7eb39adb13001eb011b30f970f8fade
quynhhchiitoo <86737884+vkng1104@users.noreply.github.com>
2023-11-05 18:09:16 +0700

    feat: add axios package and create method to checkout order

--- app/(route)/cart/components/summary.tsx
+++ app/(route)/cart/components/summary.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import axios from "axios";
+import { useSearchParams } from "next/navigation";
+
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
 import useCart from "@/hooks/use-cart";
-import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 const Summary = () => {
   const searchParams = useSearchParams();
@@ -15,6 +19,28 @@ const Summary = () => {
     0
   );
 
+  useEffect(() => {
+    if (searchParams.get("success")) {
+      toast.success("Payment completed!");
+      removeAll();
+    }
+    if (searchParams.get("failure")) {
+      toast.error("Something went wrong!");
+    }
+  }, [searchParams, removeAll]);
+
+  const onCheckout = async () => {
+    const response = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/checkout`,
+      {
+        productIds: items.map((item) => item.id),
+      }
+    );
+
+    window.location = response.data.url;
+    console.log(response.data.url);
+  };
+
   return (
     <div className="mt-16 rounded-lg bg-gray-50 px-4 py-6 sm:p-6 lg:col-span-5 lg:mt-0 lg:p-8">
       <h2 className="text-lg font-medium text-gray-900">Order Summary</h2>
@@ -24,7 +50,9 @@ const Summary = () => {
           <Currency value={totalPrice} />
         </div>
       </div>
-      <Button className="w-full mt-6">Checkout</Button>
+      <Button onClick={onCheckout} className="w-full mt-6">
+        Checkout
+      </Button>
     </div>
   );
 };
